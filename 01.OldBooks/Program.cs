30a1b1dd20454f1997f7bf27d9a9f4537c89b34b
Kristiyan Georgiev <kristiqn35@yahoo.com>
2024-07-10 16:28:10 +0300

    Add files via upload

---
+++ 01.OldBooks/Program.cs
@@ -0,0 +1,24 @@
+﻿string book = Console.ReadLine();
+string newbook=Console.ReadLine();
+int provereniKnigi = 0;
+
+
+while (newbook != "No More Books")
+{
+    if (newbook == book)
+    {
+        Console.WriteLine($"You checked {provereniKnigi} books and found it.");
+        break;
+    }
+    provereniKnigi++;
+    newbook = Console.ReadLine();
+}
+
+if (newbook== "No More Books")
+{
+    Console.WriteLine("The book you search is not here!");
+    Console.WriteLine($"You checked {provereniKnigi} books.");
+
+}
+
+
