6a206f5e5490e2b4525a709c79c3dc846d8ea4fe
Kristiyan Georgiev <kristiqn35@yahoo.com>
2024-07-10 16:29:30 +0300

    Add files via upload

---
+++ 03.Vacation/Program.cs
@@ -0,0 +1,36 @@
+﻿double neededMoney = double.Parse(Console.ReadLine());
+double ownMoney = double.Parse(Console.ReadLine());
+
+int daysCounter = 0;
+int spendCounter = 0;
+
+while (ownMoney < neededMoney)
+{
+    string input = Console.ReadLine();
+    double money = double.Parse(Console.ReadLine());
+    daysCounter++;
+    if (input == "spend")
+    {
+        spendCounter++;
+        if (spendCounter == 5)
+        {
+            Console.WriteLine("You can't save the money.");
+            Console.WriteLine(daysCounter); break;
+        }
+        ownMoney -= money;
+        if (ownMoney < 0)
+        {
+            ownMoney = 0;
+
+        }
+    }
+    else
+    {
+        spendCounter = 0;
+        ownMoney += money;
+    }
+}
+if (ownMoney >= neededMoney)
+{
+    Console.WriteLine($"You saved the money for {daysCounter} days.");
+}
---
+++ 04.Walking/Program.cs
@@ -0,0 +1,26 @@
+﻿
+string steps;
+int allSteps = 0;
+
+
+while (allSteps < 10000)
+{
+    steps = Console.ReadLine();//2000
+    if (steps == "Going home")
+    {
+        allSteps += int.Parse(Console.ReadLine());break;
+        
+    }
+
+    allSteps += int.Parse(steps);
+}
+
+if (allSteps >= 10000)
+{
+    Console.WriteLine($"Goal reached! Good job!");
+    Console.WriteLine($"{allSteps - 10000} steps over the goal!");
+}
+else 
+{
+    Console.WriteLine($"{10000 - allSteps} more steps to reach goal.");
+}
